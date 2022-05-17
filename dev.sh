c2a8ed3766090d911fc3aa5ccb7000b2f5fa59bc
Krzysztof Rzymkowski <rzymek@gmail.com>
2022-05-17 19:58:05 +0200

    tmux dev script

---
+++ dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd $(dirname "$0")
+tmux \
+	setw -g mouse on \;\
+	bind r respawn-pane -k \;\
+	bind q kill-session	\;\
+	new-session -c web "npm run start" \;\
+	split-window -c functions "npm run build -- --watch" \;\
+	split-window -fh -c functions "firebase emulators:start --only functions" \;\
+	select-pane -U
+

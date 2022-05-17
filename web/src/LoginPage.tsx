3df69fe9d2aa3b3eb841daf2f49fcaf0e426ec49
Krzysztof Rzymkowski <rzymek@gmail.com>
2022-05-17 21:07:42 +0200

    simplify LoginPage

--- web/src/LoginPage.tsx
+++ web/src/LoginPage.tsx
@@ -1,22 +1,18 @@
 import { FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import React, { useMemo } from "react";
+import React from "react";
 import { Login } from "react-admin";
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { AuthConfig } from "./AuthConfig";
 
 export function createLoginPage(firebaseApp: FirebaseApp, authConfig: AuthConfig) {
     return (props: any) => {
-        const FirebaseAuthPicker = useMemo(() =>
+        return <Login {...props}>
             <StyledFirebaseAuth firebaseAuth={getAuth(firebaseApp)} uiConfig={{
                 signInFlow: 'popup',
                 signInSuccessUrl: '#/',
                 signInOptions: authConfig.signInProviders,
-            }} />,
-            []
-        );
-        return <Login {...props}>
-            {FirebaseAuthPicker}
+            }} />
         </Login>
     };
 }
